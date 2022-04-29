c5417452ab50a8582ae80f97deb05a9eae7fe99a
jnhenkel <jeffnhenkel@gmail.com>
2022-04-29 15:18:55 -0700

    edit session based authentication

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ const NavigationBar = (props) => {
                 <div className='container-fluid'>
                     <ul id='navBar' className='navbar-nav ml-auto' >
                         {
-                            props.user && (props.user != NULL && props.user != null) ?
+                            props.user && (props.user != NULL || props.user != 'null') ?
                                 <li className='nav-item mx-2'>
                                     <NavLink className='nav-link' to='/index'>
                                         Signed in as: {props.user}
